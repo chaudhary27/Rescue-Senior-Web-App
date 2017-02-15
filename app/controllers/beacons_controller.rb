f062b2d735b96f76b091cfb002016ed30f6ab5c1
faisal farooq <chaudhary.fsl@gmail.com>
2017-02-15 09:23:02 -0500

    bug

--- app/controllers/beacons_controller.rb
+++ app/controllers/beacons_controller.rb
@@ -1,20 +1,20 @@
-require 'scan_beacon'
-class BeaconsController < ApplicationController
-
-  def initialize
-    scanner = ScanBeacon::BLE112Scanner.new
-    scanner.scan do |beacons|
-      beacons.each do |beacon|
-        puts beacon.inspect
-      end
-  end
-
-  # def scan_beacon_now
-  #   scanner = ScanBeacon::CoreBluetoothScanner.new cycle_seconds: 2
-  #   beacons = scanner.scan
-  # end
-
-end
+# require 'scan_beacon'
+# class BeaconsController < ApplicationController
+#
+#   def initialize
+#     scanner = ScanBeacon::BLE112Scanner.new
+#     scanner.scan do |beacons|
+#       beacons.each do |beacon|
+#         puts beacon.inspect
+#       end
+#   end
+#
+#   # def scan_beacon_now
+#   #   scanner = ScanBeacon::CoreBluetoothScanner.new cycle_seconds: 2
+#   #   beacons = scanner.scan
+#   # end
+#
+# end
 
 
 # to scan using the default device on mac or linux
