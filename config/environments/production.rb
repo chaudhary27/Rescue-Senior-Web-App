1482668a41c5c2cc8c82abf08fd6011db91e1c95
faisal farooq <chaudhary.fsl@gmail.com>
2017-03-17 23:39:30 -0400

    bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'rescueseniors.herokuapp.com',
     :enable_starttls_auto => true
   }
   # Settings specified here will take precedence over those in config/application.rb.
